93832555ff7479179ee2ecca8eb210e4bf1715e4
JoyIT003 <joaomarcelotecnico@outlook.com>
2025-12-03 17:01:04 -0300

    Ajuste no codigo e na mensagem final

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-// 1. Definição de Variáveis
-let nomeHeroi = "Poison";
-let xp = 10500;
+// 1. Variáveis
+let nomeHeroi = "Manoel Gomes";
+let xp = 8500;
 let nivel = "";
 
-// 2. Estrutura de Decisão (If/Else If)
-// Obs: Como usamos if/else if, não precisamos verificar o limite "inferior" 
-// de cada faixa, pois a condição anterior já o excluiu.
+// 2. Tabela de Níveis (Matriz)
+const tabelaNiveis = [
+    { limite: 1000, nome: "Ferro" },
+    { limite: 2000, nome: "Bronze" },
+    { limite: 5000, nome: "Prata" },
+    { limite: 7000, nome: "Ouro" },
+    { limite: 8000, nome: "Platina" },
+    { limite: 9000, nome: "Ascendente" },
+    { limite: 10000, nome: "Imortal" },
+];
 
-if (xp < 1000) {
-    nivel = "Ferro";
-} else if (xp >= 1001 && xp <= 2000) {
-    nivel = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
-    nivel = "Prata";
-} else if (xp >= 5001 && xp <= 7000) {
-    nivel = "Ouro";
-} else if (xp >= 7001 && xp <= 8000) {
-    nivel = "Platina";
-} else if (xp >= 8001 && xp <= 9000) {
-    nivel = "Ascendente";
-} else if (xp >= 9001 && xp <= 10000) {
-    nivel = "Imortal";
-} else {
-    // Se for maior ou igual a 10001
+// 3. Laço de Repetição para verificar o nível
+for (let i = 0; i < tabelaNiveis.length; i++) {
+    if (xp <= tabelaNiveis[i].limite) {
+        nivel = tabelaNiveis[i].nome;
+        break; 
+    }
+}
+
+// Se passou de 10.000 (o laço terminou e não encontrou nada)
+if (nivel === "") {
     nivel = "Radiante";
 }
 
-// 3. Saída
-console.log(`O nivel do heroi ${nomeHeroi} é ${nivel}`);
+// 4. Saída (Atualizada)
+console.log(`O Nível do Herói ${nomeHeroi} é ${nivel}`);
